105c300b48e4745f09e8c26453382f95fbd676a8
Christopher Wellons <wellons@nullprogram.com>
2023-12-29 00:00:24 -0500

    New runtime library: libmemory
    This static library provides memset, memcpy, memmove, memcmp, and
    strlen. The implementations are minimal wrappers around x86 string
    instructions, making them both tiny (~25 bytes apiece) and reasonably
    performant, though not nearly as fast as CRT implementations. I selected
    these particular functions because GCC fabricates calls to each out of
    thin air at some optimization levels. The first four are also genuinely
    useful as built-ins (__builtin_memset, etc.), keeping in mind arbitrary
    limitations with null.
    
    Mingw-w64 does not implement them but instead offers imports from system
    DLLs: -lmsvcrt, -lmsvcr120, -lucrt, -lntdllcrt. It creates a dependency
    on a system DLL: practical, though unoptimal, and no licensing woes.
    With -nostartfiles instead of -nostdlib, this happens implicitly.
    
    MSVC provides static definitions in libvcuntime.lib, useful when cl or
    clang-cl fabricates calls. They weigh several KB each, and require the
    application or installer to present a EULA ("external end users to agree
    to terms"). Not a great trade-off just for some basic memory functions.
    
    In w64devkit, libmemory fills this role. It's a public domain library so
    there are no license terms. Just add -lmemory to the build command. It
    also allows liberal use of the associated built-ins, especially in debug
    builds, which can benefit from fast memory operations despite -O0. MSVC
    toolchains can also freely use libmemory.a, though it won't know how to
    find it without help of course.
    
    I do not plan to add more functions except for cases of GCC fabricating
    calls (e.g. strlen). Certainly no null-terminated string functions.

---
+++ src/libmemory.c
@@ -0,0 +1,94 @@
+#if 0
+# memset, memcpy, memmove, and memcmp via x86 string instructions
+# Execute this source with a shell to build libmemory.a.
+# This is free and unencumbered software released into the public domain.
+set -e
+CFLAGS="-Os -fno-builtin -fno-asynchronous-unwind-tables -fno-ident"
+objects=""
+for func in memset memcpy memmove memcmp strlen; do
+    FUNC="$(echo $func | tr '[:lower:]' '[:upper:]')"
+    objects="$objects $func.o"
+    (set -x; ${CC:-cc} -c -D$FUNC -Wa,--no-pad-sections $CFLAGS -o $func.o $0)
+done
+rm -f libmemory.a
+ar r ${DESTDIR}libmemory.a $objects
+rm $objects
+exit 0
+#endif
+
+typedef __SIZE_TYPE__ size_t;
+
+#ifdef MEMSET
+void *memset(void *dst, int c, size_t len)
+{
+    void *r = dst;
+    asm volatile (
+        "rep stosb"
+        : "+D"(dst), "+c"(len)
+        : "a"(c)
+        : "memory"
+    );
+    return r;
+}
+#endif
+
+#ifdef MEMCPY
+void *memcpy(void *restrict dst, void *restrict src, size_t len)
+{
+    void *r = dst;
+    asm volatile (
+        "rep movsb"
+        : "+D"(dst), "+S"(src), "+c"(len)
+        :
+        : "memory"
+    );
+    return r;
+}
+#endif
+
+#ifdef MEMMOVE
+void *memmove(void *dst, void *src, size_t len)
+{
+    void *r = dst;
+    if ((size_t)dst > (size_t)src) {
+        dst += len - 1;
+        src += len - 1;
+        asm ("std");
+    }
+    asm volatile (
+        "rep movsb; cld"
+        : "+D"(dst), "+S"(src), "+c"(len)
+        :
+        : "memory"
+    );
+    return r;
+}
+#endif
+
+#ifdef MEMCMP
+int memcmp(void *s1, void *s2, size_t len)
+{
+    int a, b;
+    asm volatile (
+        "test %%eax, %%eax; repz cmpsb"
+        : "+D"(s1), "+S"(s2), "+c"(len), "=@cca"(a), "=@ccb"(b)
+        :
+        : "memory"
+    );
+    return b - a;
+}
+#endif
+
+#ifdef STRLEN
+size_t strlen(char *s)
+{
+    size_t n = -1;
+    asm volatile (
+        "repne scasb"
+        : "+D"(s), "+c"(n)
+        : "a"(0)
+        : "memory"
+    );
+    return -n - 2;
+}
+#endif
